2512792b2afea195d808cbd6e7b72b5f66175367
stanil77 <stanil@me.com>
2014-07-07 11:47:18 +0300

    new added

---
+++ modules/blockbanner/translations/bg.php
@@ -0,0 +1,19 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+
+$_MODULE['<{blockbanner}prestashop>blockbanner_4b92fcfe6f0ec26909935aa960b7b81f'] = 'Банер блок';
+$_MODULE['<{blockbanner}prestashop>blockbanner_9d9becee392c0fbcc66ff4981b8ae2f7'] = 'Показва банер в горната част на магазина.';
+$_MODULE['<{blockbanner}prestashop>blockbanner_126b21ce46c39d12c24058791a236777'] = 'невалидна снимка';
+$_MODULE['<{blockbanner}prestashop>blockbanner_df7859ac16e724c9b1fba0a364503d72'] = 'възникна грешка при качването на файла';
+$_MODULE['<{blockbanner}prestashop>blockbanner_efc226b17e0532afff43be870bff0de7'] = 'Настройките са обновени';
+$_MODULE['<{blockbanner}prestashop>blockbanner_f4f70727dc34561dfde1a3c529b6205c'] = 'Настройки';
+$_MODULE['<{blockbanner}prestashop>blockbanner_89ca5c48bbc6b7a648a5c1996767484c'] = 'Изображение на рекламния блок';
+$_MODULE['<{blockbanner}prestashop>blockbanner_18f2ae2bda9a34f06975d5c124643168'] = 'Описание на изображенито';
+$_MODULE['<{blockbanner}prestashop>blockbanner_112f6f9a1026d85f440e5ca68d8e2ec5'] = 'Моля, въведете кратко, но съдържателно описание за банера.';
+$_MODULE['<{blockbanner}prestashop>blockbanner_c9cc8cce247e49bae79f15173ce97354'] = 'Запази';
+$_MODULE['<{blockbanner}prestashop>form_92fbf0e5d97b8afd7e73126b52bdc4bb'] = 'Изберете файл';
+
+
+return $_MODULE;
---
+++ modules/blocktopmenu/translations/bg.php
@@ -0,0 +1,37 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_e5b7525b4214a759876af4448bd6b87d'] = 'Хоризонтално меню';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_efc226b17e0532afff43be870bff0de7'] = 'Настройките са обновени';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_5d1994bf28899a87795983875a98c2e1'] = 'Не е възможна актуализация на настройките.';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_e8d6809226ab177013e0a26bd2d8b60d'] = 'Моля, добави етикет за езика по подразбиране.';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_3da9d5745155a430aac6d7de3b6de0c8'] = 'Линкът беше добавен.';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_bf24faeb13210b5a703f3ccef792b000'] = 'Всички производители';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_ecf253735ac0cba84a9d2eeff1f1b87c'] = 'Всички доставчици';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'Разрешено';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_b9f5c797ebbf55adccdd8539a65a0241'] = 'Забранено';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_c9cc8cce247e49bae79f15173ce97354'] = 'Запази';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_242e9577826290307cd3e45a259172cc'] = 'Модификациите ще бъдат приложени за';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_fb54f3c5992b96d001bb16e8e92d968d'] = 'магазин';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_540c8f06b80b4ec911505764eb455f4d'] = 'всички магазини';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_58e9b25bb2e2699986a3abe2c92fc82e'] = 'Прибави нов линк';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_b021df6aac4654c454f46c77646e745f'] = 'Етикет';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_97e7c9a7d06eac006a28bf05467fcc8b'] = 'Връзка';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_e5dc8e5afea0a065948622039358de37'] = 'Нов прозорец';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_ec211f7c20af43e742bf2570c3cb84f9'] = 'Добави';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_06933067aafd48425d67bcb01bba5cb6'] = 'Обнови';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_ec136b444eede3bc85639fac0dd06229'] = 'Доставчик';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_c0bd7654d5b278e65f21cf4e9153fdb4'] = 'Производител';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_af1b98adf7f686b84cd0b443e022b7a0'] = 'Категории';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_12a521af593422cd508f7707662c9eb2'] = 'Магазини';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_068f80c7519d0528fb08e82137a72131'] = 'Продукти';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_778118c7dd993db08f704e15efa4a7fa'] = 'Избери ID на продукта';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_56e8bf6c54f1638e7bce5a2fcd5b20fe'] = 'Линкове в горното меню';
+$_MODULE['<{blocktopmenu}prestashop>blocktopmenu_387a8014f530f080bf2f3be723f8c164'] = 'Списък с линкове';
+$_MODULE['<{blocktopmenu}prestashop>form_1063e38cb53d94d386f21227fcd84717'] = 'Премахни';
+$_MODULE['<{blocktopmenu}prestashop>form_ec211f7c20af43e742bf2570c3cb84f9'] = 'Добави';
+
+
+return $_MODULE;
